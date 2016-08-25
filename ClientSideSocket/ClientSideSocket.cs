e655baba0f6408b2dee127cc2ad9d98cc5b720cc
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2016-08-25 13:01:08 +0200

    Copy of Exercise2

---
+++ ClientSideSocket/ClientSideSocket.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+
+namespace ClientSideSocket
+{
+    class ClientSideSocket
+    {
+        private string serverName;
+        private int port;
+        private volatile bool stop;
+
+        public ClientSideSocket(string serverName, int port)
+        {
+            this.serverName = serverName;
+            this.port = port;
+        }
+
+        internal void Run()
+        {
+            Console.WriteLine(GetConnectionInfo());
+            TcpClient server = new TcpClient(serverName, port);
+
+            NetworkStream stream = server.GetStream();
+            StreamWriter writer = new StreamWriter(stream);
+            StreamReader reader = new StreamReader(stream);
+
+            writer.WriteLine("Time?");
+            writer.Flush();
+
+            
+            string serverText = reader.ReadLine();
+            Console.WriteLine(serverText);
+
+            Console.WriteLine("Shutting down connection...");
+            writer.Close();
+            reader.Close();
+            stream.Close();
+            server.Close();
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+
+        private string GetConnectionInfo()
+        {
+            string connectionInfo = "This client is connected to " + serverName + " at port " + port + ".";
+            return connectionInfo;
+        }
+    }
+}
